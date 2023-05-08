061a021cb17b97f91a756ace44c132ca2ded7945
aliwa Mac <ousmane.wattara92@gmail.com>
2023-05-07 23:16:02 -0400

    Save

--- code/heur/Solver.h
+++ code/heur/Solver.h
@@ -4,17 +4,48 @@
 
 #include "../Data.h"
 #include "../Solution.h"
+#include "../Prompt.h"
+#include "../InsertOperator.h"
+#include "../BestSolutionList.h"
 #include <vector>
+
 
 class Solver {
 public:
-    Solver(Data *data):_data(data){
+    explicit Solver(Data *data):_data(data){
         TimeSlot::myData = *data;
     }
     void run();
-    static void SolveInstance(Sol &s,Data &dat);
+    static void SolveInstance(Sol &s,Data &dat,int iter);
+    static void SolveGrasp(Sol &s,Data &dat,std::vector<std::set<int>> const & linkedClientSlot);
+    static void findCorrelation(Data &dat, std::vector<TimeSlot> const & listInt, std::vector<std::set<int>> & linkedClientSlot,
+    std::vector<std::set<int>> & linkedClientDemand, std::vector<std::set<int>>& linkedClients);
+    static void findDisjointSet(Data &dat, std::vector<std::set<int>> const & linkedClients);
+    static std::vector<std::set<int>> disjointClients;
+
+//    static std::vector<grasp_insert_operator<Node,Driver>> listOperators;
+    static void fillOperatorList(Data &dat);
+    static void PathRelinking(Sol &new_cur, std::vector<Customer *> &list_cust,
+                              std::vector<BestSolutionList<Customer, Driver>>  & vectSolutions,
+                              Cost &bestCout, Sol &best);
+    static bool InsertDel(Sol &sol_cur, Sol *cur, Delivery *del);
+    static void repairSolution(Sol &new_cur);
+    static void repairSchedule(Sol &sol);
+    static std::vector<InsertOperator<Node,Driver>*> listOperators;
+
+    static void deleteOperator(){
+
+        while(listOperators.size()>0){
+            delete listOperators[0]; // Explicitly delete the pointer
+            listOperators.erase(listOperators.begin());
+        }
+    }
+
+
+
 private:
     Data *_data;
+
 };
 
 
