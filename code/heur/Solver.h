1d57a697831f736c6fe8c3a8c7f49ca1f2562423
aliwa Mac <ousmane.wattara92@gmail.com>
2023-05-26 17:47:40 -0400

    Save

--- code/heur/Solver.h
+++ code/heur/Solver.h
@@ -8,6 +8,7 @@
 #include "../Combinatorial.h"
 #include "../InsertOperator.h"
 #include "../BestSolutionList.h"
+#include "AllInsertionOperator.h"
 #include "RechercheLocale.h"
 #include "../GRASP.h"
 #include <vector>
@@ -18,15 +19,7 @@
 class Solver {
 public:
     explicit Solver(Data *data):_data(data){
-        Sol::FailureCause.resize(_data->GetNodeCount(), Parameters::FAILURECAUSE::NONE);
-        Sol::FailureCount.resize(_data->GetNodeCount(), 0);
-        Sol::StartBefore.resize(_data->GetNodeCount(), 0);
-        Sol::pullVisit.resize(_data->GetNodeCount(), 0);
-        Sol::pushVisit.resize(_data->GetNodeCount(), 0);
-        Sol::TabuFleet.resize(_data->GetNodeCount());
         CDPSolver::disjointClients.resize(_data->GetCustomerCount(), std::set < int > ());
-        Sol::minDelay.resize(_data->GetNodeCount(), 0);
-        TimeSlot::myData = *_data;
     }
     void run();
     static void SolveGrasp(Sol &s,Data &dat,std::vector<std::set<int>> const & linkedClientSlot);
@@ -41,7 +34,10 @@ private:
     void run2(Sol &s,const std::set<int>& feasible);
     void TestRecursive(Sol &cur, Delivery *del,int cap);
     void TestRecursive2(Sol &cur, Delivery *del,std::unordered_map<std::string, Sol, MyHashFunction> * umap);
+    void Test0();
+    void SaveResults(Sol &s);
     Data *_data;
+
 };
 
 
